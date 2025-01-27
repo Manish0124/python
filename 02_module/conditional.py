c1315b39f06faae2ae156b033be9297c6ff060af
Manish <techno8427@gmail.com>
2025-01-28 01:12:33 +0530

    chore: question added

--- 02_module/conditional.py
+++ 02_module/conditional.py
@@ -9,3 +9,19 @@
 # else :
 #     print("senior")
 
+
+# What will be the output of the following code if the `age` is set to 11 and the `day` is "wednesday" and provide a discount of 2 if day is wednesday?
+
+# age = 11
+
+# day = "wednesday"
+
+# price = 12 if age>=18 else 8
+
+# if day == "wednesday":
+#     price-=2
+
+
+
+# print(price)
+
