b1bf91a5f457f3786d690e813f0d9fac1ac72b55
Manish <techno8427@gmail.com>
2025-01-27 23:46:18 +0530

    conditional added

--- 01_module/hello_adrak.py
+++ 01_module/hello_adrak.py
@@ -4,3 +4,8 @@ def chai(n):
 
 
 chai("Chai ban gayi hai");
+
+chai_one = "adrak chai "
+chai_two = "lemon tea"
+chai_three = "ginger tea"
+chai_four = "masala tea"
---
+++ 02_module/conditional.py
@@ -0,0 +1,11 @@
+# age = 50
+
+# if age<20:
+#     print("child")
+# elif age<25:
+#     print("seniour")
+# elif age <50:
+#     print("adult")
+# else :
+#     print("senior")
+
